117bbc4b8699c3d8dab81f264b613cfba11b0a9b
yugi3105 <52657451+yugi3105@users.noreply.github.com>
2019-07-08 16:12:31 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,1 +1,17 @@
 
+s=input()
+l=[]
+l.append(s[0])
+maxi=0
+for i in range(len(s)):
+    for j in range(i,len(s)):
+        if s[j] not in l:
+            l.append(s[j])
+        else:
+            break
+    maxi=max(maxi,len(l))
+    l=[]
+    l.append(s[i])
+print(maxi)
+        
+        
